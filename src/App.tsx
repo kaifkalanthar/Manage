00a24bd4d8142a432cbd91dafac2e8ea9562655a
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-26 03:05:42 +0530

    features

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Features from "./components/Features"
 import HeroSection from "./components/HeroSection"
 import NavBar from "./components/NavBar"
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <NavBar />
       <HeroSection />
+      <Features />
     </>
   )
 }
